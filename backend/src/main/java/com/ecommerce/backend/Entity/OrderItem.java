97cba0be4b9b777db47a06c1d5c25ba9db8c122f
HariPrasath-369 <hariprasathmano2004@gmail.com>
2026-03-04 19:35:20 +0530

    Order functionalities order item  are added again

--- backend/src/main/java/com/ecommerce/backend/Entity/OrderItem.java
+++ backend/src/main/java/com/ecommerce/backend/Entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.ecommerce.backend.Entity;
 
+
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -23,22 +24,11 @@ public class OrderItem {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
-<<<<<<< HEAD
-    private ProductEntity product;
-=======
     private Product product;
->>>>>>> 5a21c8521f1673adfabc557e60a97291df264969
 
     @Column(nullable = false)
     private Integer quantity;
 
-<<<<<<< HEAD
-    @Column(nullable = false, precision = 10, scale = 2)
-    private BigDecimal priceAtPurchase;
-
-}
-=======
     @Column(name = "price_at_purchase", nullable = false, precision = 10, scale = 2)
     private BigDecimal priceAtPurchase;
 }
->>>>>>> 5a21c8521f1673adfabc557e60a97291df264969

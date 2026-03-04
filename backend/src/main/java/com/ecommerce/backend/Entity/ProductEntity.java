f8f009393efe82fd943f36903afbec91b8a5fcc1
MythiliV15 <150936330+MythiliV15@users.noreply.github.com>
2026-03-04 14:32:14 +0530

    Product Management

--- backend/src/main/java/com/ecommerce/backend/Entity/ProductEntity.java
+++ backend/src/main/java/com/ecommerce/backend/Entity/ProductEntity.java
@@ -1,40 +1,5 @@
 package com.ecommerce.backend.Entity;
 
 public class ProductEntity {
-    private String name;
-    private String description;
-    private double price;
-
-    public ProductEntity() {
-    }
-
-    public ProductEntity(String name, String description, double price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    
 }

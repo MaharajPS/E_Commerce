4685e194d9f4f5ac58189407893b6271117e5a04
HariPrasath-369 <hariprasathmano2004@gmail.com>
2026-03-04 21:56:04 +0530

    Added the frontend for Orders

---
+++ frontend/src/pages/CheckoutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Checkout from '../components/orders/Checkout';
+
+const CheckoutPage = () => {
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-8">
+      <Checkout />
+    </div>
+  );
+};
+
+export default CheckoutPage;

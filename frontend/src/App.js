aed18fad9ffcb0e125a966d8e248df51cc3a5b60
Maharaj P S <maharajpsd@gmail.com>
2026-03-04 22:25:41 +0530

    Created Routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/common/Navbar';
-
+import ProtectedRoute from './components/common/ProtectedRoute';
 
 // Pages
 import HomePage from './pages/HomePage';
@@ -10,7 +10,11 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrdersPage from './pages/OrdersPage';
+import AdminPage from './pages/AdminPage';
+import AnalyticsPage from './pages/AnalyticsPage';
 
 function App() {
   return (
@@ -23,10 +27,29 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
-              
+
               <Route path="/products" element={<ProductsPage />} />
               <Route path="/products/:id" element={<ProductDetailPage />} />
-              
+
+              <Route path="/cart" element={
+                <ProtectedRoute><CartPage /></ProtectedRoute>
+              } />
+
+              <Route path="/checkout" element={
+                <ProtectedRoute><CheckoutPage /></ProtectedRoute>
+              } />
+
+              <Route path="/orders" element={
+                <ProtectedRoute><OrdersPage /></ProtectedRoute>
+              } />
+
+              <Route path="/admin" element={
+                <ProtectedRoute adminOnly><AdminPage /></ProtectedRoute>
+              } />
+
+              <Route path="/analytics" element={
+                <ProtectedRoute adminOnly><AnalyticsPage /></ProtectedRoute>
+              } />
             </Routes>
           </main>
         </div>
